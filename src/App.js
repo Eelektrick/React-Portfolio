35e3593d0874b4ae3b571552f51ac28bc64c87a4
Corey Post <corey.post0120@gmail.com>
2020-10-16 00:01:34 -0600

    added and re arranged code to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
-import Gallery from "./pages/Gallery";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Header />
       <Wrapper>
-        <Gallery />
+        <Route exact path="/" component={Index}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/portfolio" component={Portfolio}/>
+        <Route exact path="/contact" component={Contact}/>
       </Wrapper>
       <Footer />
     </div>
