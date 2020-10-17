90b3bc5ddfc214fe1122553576d43027f1616085
Corey Post <corey.post0120@gmail.com>
2020-10-17 15:54:11 -0600

    created intro component to work with index page

---
+++ src/components/Intro/Hello.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Hello() {
+  return (
+    // <!-- Intro about me -->
+    <div className="container-fluid">
+      <div className="row">
+        <div className="text-center col-md-12 text-white" id="size">
+          Corey Post
+          <br /> I'm a Full Stack Web Developer
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hello;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,15 +1,12 @@
 import React from "react";
+import Intro from "../components/Intro/Hello";
+import Wrapper from "../components/Wrapper";
 
 function Index(){
     return(
-        // <!-- Intro about me -->
-        <div className="container-fluid">
-            <div className="row">
-                <div className="text-center col-md-12 text-white" id="size">
-                Corey Post<br /> I'm a Full Stack Web Developer
-                </div>
-            </div>
-        </div>
+        <Wrapper>
+            <Intro />
+        </Wrapper>
     )
 }
 
