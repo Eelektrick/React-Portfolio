5e6852c598a4a0f59f9a71b836520742ac73915c
Corey Post <corey.post0120@gmail.com>
2020-10-17 11:59:09 -0600

    added code to index page

---
+++ src/pages/Index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Index(){
+    return(
+        // <!-- Intro about me -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="text-center col-md-12 text-white" id="size">
+                Corey Post<br /> I'm a Full Stack Web Developer
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Index;
