228b67e61c3e9d296bba91ee5c48c1faa85ab030
Corey Post <corey.post0120@gmail.com>
2020-10-17 21:38:46 -0600

    updated navbar links to work in react

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,35 +1,48 @@
-import React from "react";
+import React, {Component} from "react";
 import "./style.css";
 import Resume from "./CoreyPostResume.pdf";
 import {HashLink as Link} from "react-router-hash-link";
+import { render } from "@testing-library/react";
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light absolute-top">
-      <h1 id="fancy">Corey Post</h1>
-      <button className="navbar-toggler btn btn-light" id="tButton" type="button" data-toggle="collapse" data-target="#navbarMenu">
-          <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarMenu">
-          <ul className="navbar-nav ml-auto navFont">
-              <li className="nav-item">
-                  <a className="nav-link text-white" to="/about">About Me</a>
-              </li>
-              <li className="nav-item">
-                {/*Found the code to open resume in a different tab from stack overflow*/}
-                  <a className="nav-link text-white" target="_blank" rel="noopener noreferrer" href={Resume}>Resume</a>
-              </li>
-              <li className="nav-item">
-                  <a className="nav-link text-white" to="/portfolio">Portfolio</a>
-              </li>
-              <li className="nav-item">
-                  <a className="nav-link text-white" to="/contact">Contact</a>
-              </li>
-          </ul>
-      </div>
-    </nav>
-  );
+class Navbar extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      menu: false
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+  toggleMenu(){
+    this.setState({ menu: !this.state.menu })
+  }
+  render() {
+    const show = (this.state.menu) ? "show" : "" ;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light absolute-top">
+        <h1 id="fancy">Corey Post</h1>
+        <button className="navbar-toggler btn btn-light" id="tButton" type="button" data-toggle="collapse" data-target="#navbarMenu">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={"collapse navbar-collapse" + show} id="navbarMenu">
+            <ul className="navbar-nav ml-auto navFont">
+                <li className="nav-item nav-link text-white">
+                    <Link to="/about" className={window.location.pathname === "/about" || window.location.pathname === "/about" ? "nav-link active" : "nav-link"}>About Me</Link>
+                </li>
+                <li className="nav-item nav-link text-white">
+                  {/*Found the code to open resume in a different tab from stack overflow*/}
+                    <Link className="nav-link text-white" target="_blank" rel="noopener noreferrer" href={Resume}>Resume</Link>
+                </li>
+                <li className="nav-item nav-link text-white">
+                    <Link to="/portfolio" className={window.location.pathname === "/portfolio" || window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>Portfolio</Link>
+                </li>
+                <li className="nav-item nav-link text-white">
+                    <Link to="/contact" className={window.location.pathname === "/contact" || window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>Contact</Link>
+                </li>
+            </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
