9996860b84156e11288a04cb86959cfef33ab42f
Corey Post <corey.post0120@gmail.com>
2020-10-17 21:50:44 -0600

    updated navbar code for links to work

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,6 @@ import React, {Component} from "react";
 import "./style.css";
 import Resume from "./CoreyPostResume.pdf";
 import {HashLink as Link} from "react-router-hash-link";
-import { render } from "@testing-library/react";
 
 class Navbar extends Component{
   constructor(props){
@@ -20,23 +19,23 @@ class Navbar extends Component{
     return (
       <nav className="navbar navbar-expand-lg navbar-light absolute-top">
         <h1 id="fancy">Corey Post</h1>
-        <button className="navbar-toggler btn btn-light" id="tButton" type="button" data-toggle="collapse" data-target="#navbarMenu">
+        <button className="navbar-toggler btn btn-light" id="tButton" type="button" onClick={this.toggleMenu}>
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className={"collapse navbar-collapse" + show} id="navbarMenu">
             <ul className="navbar-nav ml-auto navFont">
-                <li className="nav-item nav-link text-white">
-                    <Link to="/about" className={window.location.pathname === "/about" || window.location.pathname === "/about" ? "nav-link active" : "nav-link"}>About Me</Link>
+                <li className="nav-item nav-link">
+                    <Link to="/about" className={window.location.pathname === "/about" || window.location.pathname === "/about" ? "nav-link active" : "nav-link text-white"}>About Me</Link>
                 </li>
-                <li className="nav-item nav-link text-white">
+                <li className="nav-item nav-link">
                   {/*Found the code to open resume in a different tab from stack overflow*/}
                     <Link className="nav-link text-white" target="_blank" rel="noopener noreferrer" href={Resume}>Resume</Link>
                 </li>
-                <li className="nav-item nav-link text-white">
-                    <Link to="/portfolio" className={window.location.pathname === "/portfolio" || window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>Portfolio</Link>
+                <li className="nav-item nav-link">
+                    <Link to="/portfolio" className={window.location.pathname === "/portfolio" || window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link text-white"}>Portfolio</Link>
                 </li>
-                <li className="nav-item nav-link text-white">
-                    <Link to="/contact" className={window.location.pathname === "/contact" || window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>Contact</Link>
+                <li className="nav-item nav-link">
+                    <Link to="/contact" className={window.location.pathname === "/contact" || window.location.pathname === "/contact" ? "nav-link active" : "nav-link text-white"}>Contact</Link>
                 </li>
             </ul>
         </div>
