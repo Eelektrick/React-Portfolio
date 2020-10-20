213f8456b35277965fb9276883b6646850d985ba
Corey Post <corey.post0120@gmail.com>
2020-10-19 19:01:16 -0600

    update pages and component for portfolio

---
+++ src/components/Project/Projects.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+function Projects(){
+    return(
+        <div className="container">
+            <h1 className="text-white border-bottom border-light">Portfolio</h1>
+
+            <div className="row">
+                <div className="p-4 col-md-6">
+                    {/* {images} */}
+                    <div className="text-center">
+                        {/* {Title} */}
+                        {/* {website link} */}
+                        {/* {modal link} */}
+                        {/* {Github link} */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Projects;
