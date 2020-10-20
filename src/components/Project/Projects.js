c784d36e0f516192f0c347684345a498d190a914
Corey Post <corey.post0120@gmail.com>
2020-10-19 20:25:25 -0600

    added images to json file and project component and added lodash

--- src/components/Project/Projects.js
+++ src/components/Project/Projects.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React, {Component} from "react";
 import "./style.css";
+import projects from "./Projects.Json";
+import a from "./media/bartender.PNG";
+import b from "./media/finalConcept.png";
+import c from "./media/dashboard.PNG";
+import d from "./media/screenshot.PNG";
+import e from "./media/scheduler.PNG";
+import f from "./media/notes.PNG";
+import _ from "lodash";
 
 function Projects(){
     return(
